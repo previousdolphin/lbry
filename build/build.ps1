e6c6761990829f960aff2b1bd2297845ac2892c7
Jack Robison <jackrobison@lbry.io>
2018-01-29 13:18:46 -0500

    debug

--- build/build.ps1
+++ build/build.ps1
@@ -5,9 +5,11 @@ gcc --version
 mingw32-make --version
 
 # build/install miniupnpc manually
+echo "building miniupnpc"
 tar zxf miniupnpc-1.9.tar.gz
 cd miniupnpc-1.9
 mingw32-make -f Makefile.mingw
+echo "installing python binding for miniupnpc"
 python setupmingw32.py build --compiler=mingw32
 python setupmingw32.py install
 cd ..\
@@ -18,18 +20,26 @@ Get-Content ..\requirements.txt | Select-String -Pattern 'gmpy|miniupnpc' -NotMa
 # add in gmpy wheel
 Add-Content requirements.txt "./gmpy-1.17-cp27-none-win32.whl"
 
+echo "set build type"
 python set_build.py
 
+echo "installing lbrynet requirements"
 pip install -r requirements.txt
+echo "installing lbrynet"
 pip install ..\.
 
+echo "building lbrynet-daemon.exe"
 pyinstaller -y daemon.onefile.spec
+echo "building lbrynet-cli.exe"
 pyinstaller -y cli.onefile.spec
+echo "building lbrynet-console.exe"
 pyinstaller -y console.onefile.spec
 
+echo "signing binaries"
 nuget install secure-file -ExcludeVersion
 secure-file\tools\secure-file -decrypt .\lbry2.pfx.enc -secret "$env:pfx_key"
 signtool.exe sign /f .\lbry2.pfx /p "$env:key_pass" /tr http://tsa.starfieldtech.com /td SHA256 /fd SHA256 dist\*.exe
 
+echo "uploading assets"
 python zip_daemon.py
 python upload_assets.py
